29eaf48091e12e5d8a22becc69698d5c9cdca262
Alexandru-Eugen Toma <47105019+tomaAlex@users.noreply.github.com>
2020-06-19 16:29:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ k = parseInt(k);
 
 function getIndexOfSelectedElement() {
     for (let i = 0; i < document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").childElementCount; i++)
-        if(document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-out') && 
+        if((document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-out') || document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-in')) && 
            document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].children[0].children[0].className.includes('_1GcfQ')) return i + 1;
 }
 
@@ -12,6 +12,6 @@ if(k == -1) final_index = document.querySelector("#main > div._3h-WS > div > div
 else final_index = getIndexOfSelectedElement() + k - 1;
 
 for (let i = getIndexOfSelectedElement(); i < final_index; i++)
-    if(document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-out'))
+    if(document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-out') || document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-in'))
         document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].children[0].children[0].children[0].click();
     else final_index++;
