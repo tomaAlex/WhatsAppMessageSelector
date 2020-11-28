6f65a41ebf16b3abdaa4a70a315c993b7859304f
Alexandru-Eugen Toma <47105019+tomaAlex@users.noreply.github.com>
2020-11-28 11:27:56 +0000

    Update index.js
    modified class names from queryselector method, so the elements get selected, thus the script keeps working

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ let k = prompt("How many messages do you want to select? (insert -1 if you want 
 k = parseInt(k);
 
 function getIndexOfSelectedElement() {
-    for (let i = 0; i < document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").childElementCount; i++)
-        if((document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-out') || document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-in')) && 
-           document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].children[0].children[0].className.includes('_1GcfQ')) return i + 1;
+    for (let i = 0; i < document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').childElementCount; i++)
+        if((document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-out') || document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-in')) && 
+           document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].children[0].children[0].className.includes('_3TADo')) return i + 1;
 }
 
 let final_index = 0;
-if(k == -1) final_index = document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").childElementCount;
+if(k == -1) final_index = document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').childElementCount;
 else final_index = getIndexOfSelectedElement() + k - 1;
 
 for (let i = getIndexOfSelectedElement(); i < final_index; i++)
-    if(document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-out') || document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].className.includes('message-in'))
-        document.querySelector("#main > div._3h-WS > div > div > div.z_tTQ._13ILk").children[i].children[0].children[0].children[0].click();
+    if(document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-out') || document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-in'))
+        document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].children[0].children[0].children[0].click();
     else final_index++;
