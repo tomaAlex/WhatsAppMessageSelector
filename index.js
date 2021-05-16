34c9d98b7bd82196a36d4fb94f5805d5a69b0370
Alexandru-Eugen Toma <47105019+tomaAlex@users.noreply.github.com>
2021-05-16 09:33:36 +0100

    updated paths

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let k = prompt("How many messages do you want to select? (insert -1 if you want to select the files untill the end)");
 k = parseInt(k);
+let messagesContainer = document.querySelector('#main > div._2wjK5 > div > div._1gL0z._2p49L > div._11liR._1fxaZ');
 
 function getIndexOfSelectedElement() {
-    for (let i = 0; i < document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').childElementCount; i++)
-        if((document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-out') || document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-in')) && 
-           document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].children[0].children[0].className.includes('_3TADo')) return i + 1;
+    for (let i = 0; i < messagesContainer.childElementCount; i++)
+        if((messagesContainer.children[i].className.includes('message-out') || messagesContainer.children[i].className.includes('message-in')) && 
+           messagesContainer.children[i].children[0].children[0].className.includes('_3GtSJ')) return i + 1;
 }
 
 let final_index = 0;
-if(k == -1) final_index = document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').childElementCount;
+if(k == -1) final_index = messagesContainer.childElementCount;
 else final_index = getIndexOfSelectedElement() + k - 1;
 
 for (let i = getIndexOfSelectedElement(); i < final_index; i++)
-    if(document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-out') || document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].className.includes('message-in'))
-        document.querySelector('#main > div.RUGMB > div > div > div.tSmQ1.jujkF').children[i].children[0].children[0].children[0].click();
+    if(messagesContainer.children[i].className.includes('message-out') || messagesContainer.children[i].className.includes('message-in'))
+        messagesContainer.children[i].children[0].children[0].children[0].click();
     else final_index++;
