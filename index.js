8e9ea68ed4299e18f6ae1e75ad73d16850df8bf7
Alexandru-Eugen Toma <47105019+tomaAlex@users.noreply.github.com>
2021-08-11 12:37:30 +0100

    updated paths

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let k = prompt("How many messages do you want to select? (insert -1 if you want to select the files untill the end)");
 k = parseInt(k);
-let messagesContainer = document.querySelector('#main > div._2wjK5 > div > div._1gL0z._2p49L > div._11liR._1fxaZ');
+let messagesContainer = document.querySelector('#main > div._1LcQK > div > div._33LGR._1HzbD > div.y8WcF._1RYqc');
 
 function getIndexOfSelectedElement() {
     for (let i = 0; i < messagesContainer.childElementCount; i++)
         if((messagesContainer.children[i].className.includes('message-out') || messagesContainer.children[i].className.includes('message-in')) && 
-           messagesContainer.children[i].children[0].children[0].className.includes('_3GtSJ')) return i + 1;
+           messagesContainer.children[i].children[0].children[0].className.includes('_3BK98')) return i + 1;
 }
 
 let final_index = 0;
